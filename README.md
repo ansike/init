349badb101af3daa79aed7e5e0e1ec988e68a12d
ansike <ansike@ansikedeMacBook-Pro.local>
2023-07-16 17:14:58 +0800

    feat: mac 安装软件

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 
 配置ssh
 配置git
+
+FAQ
+1. 软件安装中网络下载卡住很大的原因是没有给terminal开网络代理
+```shell
+# 以我自己的proxy为例
+export http_proxy=http://localhost:7890
+export https_proxy=http://localhost:7890
+```
--- src/install/macos/install.sh
+++ src/install/macos/install.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+set -e
+
+# 需要按照自己实际的代理走
+export http_proxy=http://localhost:7890
+export https_proxy=http://localhost:7890
+
+# TODO 应该可以删掉
+[ $SHELL = '/bin/zsh' ] && source ~/.zshrc || {
+    source ~/.bashrc
+    source ~/.profile
+    source ~/.bash_profile
+}
+
 # 检查软件是否已经安装
 check_installed() {
-    command -v "$1" &> /dev/null
+    # echo "检查命令是否存在"$1
+    command -v "$1" &>/dev/null
 }
 
 # 输出日志信息
@@ -10,78 +24,102 @@ log() {
     echo "$(date +'%Y-%m-%d %H:%M:%S') - $1"
 }
 
-# 显示进度
-show_progress() {
-    local -r pid=$1
-    local -r delay=0.5
-    local spin='-\|/'
-    local i=0
-
-    log "正在安装中，请稍候..."
-
-    while kill -0 $pid 2>/dev/null; do
-        local i=$(( (i+1) %4 ))
-        printf "\r${spin:$i:1}"
-        sleep $delay
-    done
-
-    printf "\r"
+check_version() {
+    echo "\033[32m✓ $*\033[0m"
 }
+
+##################################################### 安装&配置 ###########################################################
+
+echo "开始设置 bash alias"
+alias ..='cd ../'
+alias ...='cd ../../'
+alias ....='cd ../../../'
+alias pro='cd ～/pro'
 
 ###############
 # 检查 Xcode 是否已安装
 ###############
-if ! check_installed xcode-select; then
+order='xcode-select'
+if ! check_installed $order; then
     log "Xcode未找到，正在安装Xcode..."
 
     # 安装 Xcode Command Line Tools
     xcode-select --install &>/dev/null
 
-    # 显示安装进度
-    show_progress $!
-
-    log "Xcode安装完成。"
+    [ $? -eq 0 ] && log "${order}安装完成。" || log "${order}安装失败。"
 fi
 
-[ $? -eq 0 ] && log "Xcode pass"
+check_version $(xcode-select -v)
 
 ###############
 # 安装Homebrew（如果尚未安装）
 ###############
-if ! check_installed brew; then
-    log "正在安装Homebrew..."
+order='brew'
+if ! check_installed $order; then
+    log "正在安装$order..."
 
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &>/dev/null
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-    log "Homebrew安装完成。"
+    [ $? -eq 0 ] && log "${order}安装完成。" || log "${order}安装失败。"
 fi
 
-###############
-# 安装&配置Git
+check_version $(brew -v)
+
 ###############
 # 检查 unzip 是否已安装
-if ! check_installed unzip; then
-    log "unzip未找到，正在安装unzip..."
+###############
+order='unzip'
+if ! check_installed $order; then
+    log "$order未找到，正在安装$order..."
 
-    # 安装 unzip
-    brew install unzip &>/dev/null
+    # 安装 $order
+    brew install $order &>/dev/null
 
-    log "unzip安装完成。"
+    [ $? -eq 0 ] && log "${order}安装完成。" || log "${order}安装失败。"
 fi
+
+check_version $(unzip -v | head -n 1)
 
 ###############
 # 安装&配置Git
 ###############
-if ! check_installed git; then
-    brew install git
+order='git'
+if ! check_installed $order; then
+    brew install $order
+fi
+
+check_version $(git -v)
+
+if check_installed "git"; then
+    log "git 已安装, 开始设置 git alias"
+    git config --global alias.st status
+    git config --global alias.aa "add ."
+    git config --global alias.ck checkout
+    git config --global alias.cm "commit -m"
+    git config --global alias.br branch
+    git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+else
+    log "git 未安装, 需要安装之后重新运行该shell"
 fi
 
 ###############
 # 安装nvm
 ###############
+order='nvm'
 if ! check_installed nvm; then
+    log "正在安装 $order ..."
+
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+    [ $SHELL = '/bin/zsh' ] && source ~/.zshrc || {
+        source ~/.bashrc
+        source ~/.profile
+        source ~/.bash_profile
+    }
+    [ $? -eq 0 ] && log "${order}安装完成。" || log "${order}安装失败。"
 fi
+
+check_version "nvm" $(nvm -v)
 
 ###############
 # 检查 VSCode 是否已安装
@@ -101,19 +139,49 @@ if ! check_installed code; then
     # 清理安装文件
     rm vscode.zip
 
+    # 配置code命令
+    sudo ln -s /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code /usr/local/bin/code
+
     log "VSCode安装完成。"
 fi
 
+check_version "vscode" $(code -v)
 
-# 安装@vue/cli
-if ! check_installed vue; then
-    yarn global add @vue/cli
+###############
+# 安装 node
+###############
+order='code'
+if ! check_installed $order; then
+    nvm install 16.14.0
 fi
+
+check_version "node" $($order -v)
+check_version "npm" $(npm -v)
+
+###############
+# 安装yarn
+###############
+order="yarn"
+if ! check_installed $order; then
+    npm i -g $order
+fi
+
+check_version "yarn" $($order -v)
+
+###############
+# 安装@vue/cli
+###############
+if ! check_installed vue; then
+    yarn global -g @vue/cli
+fi
+
+check_version $(vue -V)
 
 # 安装create-react-app
 if ! check_installed create-react-app; then
     yarn global add create-react-app
 fi
+check_version "create-react-app" $(create-react-app -V)
 
 # 安装Python（使用pyenv管理多个版本）
 if ! check_installed pyenv; then
@@ -121,10 +189,10 @@ if ! check_installed pyenv; then
 
     # 添加pyenv到bashrc或者zshrc配置文件（如果尚未添加）
     if ! grep -q "pyenv init" ~/.zshrc || ! grep -q "pyenv virtualenv-init" ~/.zshrc; then
-        echo 'if command -v pyenv 1>/dev/null 2>&1; then' >> ~/.zshrc
-        echo '  eval "$(pyenv init --path)"' >> ~/.zshrc
-        echo '  eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
-        echo 'fi' >> ~/.zshrc
+        echo 'if command -v pyenv 1>/dev/null 2>&1; then' >>~/.zshrc
+        echo '  eval "$(pyenv init --path)"' >>~/.zshrc
+        echo '  eval "$(pyenv virtualenv-init -)"' >>~/.zshrc
+        echo 'fi' >>~/.zshrc
     fi
 
     # 安装最新的Python版本
@@ -136,19 +204,50 @@ if ! check_installed pyenv; then
     pyenv global $(pyenv versions --bare | tail -1)
 fi
 
+check_version $(pyenv -v)
+
 # 安装Docker（如果尚未安装）
 if ! check_installed docker; then
-    if [ ! -f "/usr/local/bin/docker" ]; then
-        curl -fsSL https://get.docker.com -o get-docker.sh
-        sudo sh get-docker.sh
-        rm get-docker.sh
-    fi
+    brew install --cask --appdir=/Applications docker
 fi
+
+check_version $(docker -v)
 
 # 安装OpenSSH（通常已经安装）
 if ! check_installed ssh; then
     brew install openssh
 fi
 
+# 检查 SSH 目录是否存在
+if [ ! -d "$HOME/.ssh" ]; then
+    log "创建 SSH 目录..."
+    mkdir "$HOME/.ssh"
+    chmod 700 "$HOME/.ssh"
+fi
+
+# 生成 SSH 公私钥
+generate_ssh_keys() {
+    local -r ssh_dir="$HOME/.ssh"
+    local -r private_key_file="$ssh_dir/id_rsa"
+    local -r public_key_file="$ssh_dir/id_rsa.pub"
+
+    if [ -f "$private_key_file" ] || [ -f "$public_key_file" ]; then
+        log "SSH 公私钥已存在。"
+    else
+        log "正在生成 SSH 公私钥..."
+        ssh-keygen -t rsa -b 4096 -f "$private_key_file" -N "" -q
+        log "SSH 公私钥已生成。"
+    fi
+
+    log "公钥内容："
+    cat "$public_key_file"
+}
+
+generate_ssh_keys
+
 # 完成提示
-echo "初始化完成！"
+check_version "初始化完成！"
+
+# 删除代理
+export http_proxy=
+export https_proxy=
